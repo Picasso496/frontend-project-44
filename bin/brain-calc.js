e43a2db23277c0c3a1c171ea1ade92ed18edbe6f
Pavel <pavel_maznev@mail.ru>
2025-11-24 01:42:08 +0500

    calc 2

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,35 +1,39 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-export default function runBrainCalc() {
-  console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('What is the result of the expression?');
-  
-  const operations = ['+', '-', '*'];
-  const roundsCount = 3;
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 
-  for (let i = 0; i < roundsCount; i++) {
-    const op = operations[Math.floor(Math.random() * operations.length)];
-    const num1 = Math.floor(Math.random() * 50) + 1;
-    const num2 = Math.floor(Math.random() * 50) + 1;
+console.log('What is the result of the expression?');
 
-    const question = `${num1} ${op} ${num2}`;
-    const correctAnswer = eval(question).toString();
+const operations = ['+', '-', '*'];
+const roundsCount = 3;
 
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ').trim();
+let correctAnswers = 0;
 
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
+for (let i = 0; i < roundsCount; i++) {
+  const op = operations[Math.floor(Math.random() * operations.length)];
+  const num1 = Math.floor(Math.random() * 50) + 1;
+  const num2 = Math.floor(Math.random() * 50) + 1;
 
+  const question = `${num1} ${op} ${num2}`;
+  const correctAnswer = eval(question).toString();
+
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ').trim();
+
+  if (userAnswer !== correctAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    break; // Просто выходим из цикла, не используем process.exit
+  } else {
     console.log('Correct!');
+    correctAnswers += 1;
   }
+};
 
+if (correctAnswers === roundsCount) {
   console.log(`Congratulations, ${userName}!`);
 };
